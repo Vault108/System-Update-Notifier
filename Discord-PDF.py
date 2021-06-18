8ed96f8f33728c60772a2cc86730a0cf8c68e4cb
Vault108 <44564111+Vault108@users.noreply.github.com>
2021-06-18 19:57:42 -0400

    Create Discord-PDF.py

---
+++ Discord-PDF.py
@@ -0,0 +1,29 @@
+import sys
+import ubelt
+from datetime import date
+from dhooks import Webhook, File
+from fpdf import FPDF
+from io import BytesIO
+
+def sun():
+    today = date.today().strftime("%F")
+    URL = ""
+    aptlist = ubelt.cmd("apt list --upgradeable")["out"]
+    out = sys.stdout
+    with open(today + ".txt", "w") as f:
+        sys.stdout = f
+        print(aptlist)
+        sys.stdout = out
+    pdf = FPDF("P", "mm", (297, 210))
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    a = open(today + ".txt", "r")
+    for x in a:
+        pdf.cell(200, 7, txt=x, ln=1)
+    pdf.output(today+".pdf")
+    hook = Webhook(URL)
+    file = File(today + ".pdf")
+    hook.send(file=file)
+
+
+sun()
