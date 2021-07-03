798786d8d7b09fbfbb367fda32629bea7a65d79a
Vault108 <Vault108@users.noreply.github.com>
2021-07-03 23:10:11 +0000

    autopep8 action fixes

--- Textbelt.py
+++ Textbelt.py
@@ -1,12 +1,11 @@
 import ubelt
 import requests
-ServerName = "" #server name
+ServerName = ""  # server name
 updates = ubelt.cmd('sudo apt update')['out'].splitlines()[-1]
 
 resp = requests.post('https://textbelt.com/text', {
-    'phone': '', #Number to Send to
+    'phone': '',  # Number to Send to
     'message': ServerName + ": " + updates,
-    'key': '', #textbelt Api key
+    'key': '',  # textbelt Api key
 })
 print(resp.json())
-
