58a5c923889abbf3bc16216ce4ebcc38d16f9190
Vault108 <44564111+Vault108@users.noreply.github.com>
2021-09-18 21:31:36 -0400

    added rate limits
    added rate limits

--- Textbelt.py
+++ Textbelt.py
@@ -2,9 +2,11 @@ import ubelt
 import requests
 from loguru import logger
 import sys
+from ratelimits import limits
 
 
 @logger.catch
+@limits(calls=1, period=120) # 1 call per 120 seconds. 
 def suntext():
     ServerName = "test"  # server name
     updates = ubelt.cmd('sudo apt update')['out'].splitlines()[-1]
